613a46b94b093be11eaef41ec2cd2c8c0eacc1b7
RupamSingh333 <rupamkumar333@gmail.com>
2022-12-25 23:57:33 +0530

    cart data ok

--- src/context/Cart_Context.js
+++ src/context/Cart_Context.js
@@ -5,10 +5,15 @@ const CartContext = createContext();
 
 const getLocalCartData = () => {
   let localCartData = localStorage.getItem("CartItem");
-  if (localCartData === []) {
-    return [];
-  } else {
-    return JSON.parse(localCartData);
+  // if (localCartData === []) {
+  //   return [];
+  // } else {
+  //   return JSON.parse(localCartData);
+  // }
+
+  const parseData = JSON.parse(localCartData);
+  if(!Array.isArray(parseData)) return [];{
+    return parseData;
   }
 };
 
